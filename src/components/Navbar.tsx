d6a2da70f4958daee4f8dcead62b205ce94033fa
SAAD <Maissa.Saad@etu.sorbonne-universite.fr>
2026-07-24 13:54:47 +0200

    logo update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
 import Container from "@/components/Container";
 import Link from "next/link";
 
+
 export default function Navbar() {
   return (
     <header className="fixed top-0 z-50 w-full border-b border-slate-800 bg-slate-950/90 backdrop-blur-xl">
       <Container>
         <nav className="flex h-16 items-center justify-between">
 
-          <h1 className="text-2xl font-bold tracking-tight text-white">
+          <Link
+            href="/"
+            className="text-xl font-bold tracking-tight transition hover:opacity-80"
+          >
             <span className="text-violet-400">M</span>aïssa Saad
-          </h1>
+          </Link>
 
           <div className="flex gap-10 text-sm font-medium text-slate-400">
 
